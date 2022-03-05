e77aa42cb5bf9f773db42186fa58a6162def9b5e
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-05 21:00:41 +0300

    i write some statemet select date input and give it js format ,select gender input ,add eventlistener to submit ,there after i can incorporate the statement

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 //select input of date from form
 let input = document.getElementById("date").value;
+console.log(input);
 let dateEntered = new Date(input);
+console.log(dateEntered);
 
 //select gender either male or female from form
 let gender = document.querySelectorAll('input[name=gender]');
+console.log(gender);
 
 //Define form=>fillin
 const fillinform = document.querySelector("fillin");
+console.log(fillinform);
 
-//add eventlistener to form
-fillinform.addEventListener("submit", validateUserInfo);
+//validate radio buttons
+if((document.fillin.gender[0].checked == false) && (document.fillin.gender[0].checked == false)){
+    alert("Please choose your Gender either Male or Female");
+}
+
+
+// //add eventlistener to form
+// fillinform.addEventListener("submit", validateUserInfo);
 
 // create the function to validate the input when user
 // clicks submit button
@@ -18,6 +28,8 @@ function validateUserInfo(event){
     //to avoid default refresh
     event.preventDefault();
 }
+
+
 
 
 
