69ff7f90da8ab0354b3f34ecb90291e0e9e06e3c
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-06 01:32:10 +0300

    I have debug the bugs or blockers i add and incorporated a few statement so that my function can run smoothly

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
 //creating an array
 const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+//select gender either male or female from form
+let gender = document.querySelectorAll('input[name=gender]');
+
+//initilaze form=>fillin
+const fillinform = document.querySelector("#fillin");
+
 // let genderM = fillinform.gender_male.value;
 // let genderF = fillinform.gender_female.value;
 
-//initilaze form=>fillin
-const fillinform = document.querySelector("fillin");
+document.getElementById("submit").addEventListener("submit", validateUserInfo =>{
+    //to avoid default refresh
+    event.preventDefault();
+    //select input of date from form
+    let input = document.getElementById("date").value;
+    let dateEntered = new Date(input);
+    //To ge the day
+    const day = dateEntered.getDate();
+    console.log(dayOfTheWeek[day]);
+    chose();
+});
 
-// //add eventlistener to form
-// fillinform.addEventListener("submit", validateUserInfo);
-document.getElementById("submit").addEventListener("submit" validateUserInfo =>{
-     //select input of date from form
-     let input = document.getElementById("date").value;
-     let dateEntered = new Date(input);
-     //To ge the day
-     const day = dateEntered.getDate();
-     console.log(dayOfTheWeek[day]);
-})
-
-
-// function validateUserInfo(event){
-//     //to avoid default refresh
-//     event.preventDefault();
-   
-// }
-
-
-//select gender either male or female from form
-let gender = document.querySelectorAll('input[name=gender]');
-console.log(gender);
-
-//select form
-const form = document.querySelectorAll("form");
-console.log(form);
-
-
-
-
-//validate radio buttons
-if(genderM .checked == false && genderF.checked == false){
-    console.log("Please choose your Gender either Male or Female");
+function chose(male , female){
+    //validate radio buttons
+    if(genderM .checked == false && genderF.checked == false){
+        return("Please choose your Gender either Male or Female");
+    }
 }
 
-
-
-
-// create the function to validate the input when user
-// clicks submit button
 
 
 
