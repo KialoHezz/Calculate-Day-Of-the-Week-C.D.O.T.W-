609f5d35fd2a20d90362bc3f7e67d6b4e9010fb8
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-05 22:28:27 +0300

    create index.js has statement in it to be incorporated and function to validate when user clicks submit

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-//select input of date from form
-let input = document.getElementById("date").value;
-console.log(input);
-let dateEntered = new Date(input);
-console.log(dateEntered);
+//creating an array
+const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// let genderM = fillinform.gender_male.value;
+// let genderF = fillinform.gender_female.value;
+
+//initilaze form=>fillin
+const fillinform = document.querySelector("fillin");
+
+// //add eventlistener to form
+// fillinform.addEventListener("submit", validateUserInfo);
+document.getElementById("submit").addEventListener("submit" validateUserInfo =>{
+     //select input of date from form
+     let input = document.getElementById("date").value;
+     let dateEntered = new Date(input);
+     //To ge the day
+     const day = dateEntered.getDate();
+     console.log(dayOfTheWeek[day]);
+})
+
+function validateUserInfo(event){
+    //to avoid default refresh
+    event.preventDefault();
+   
+}
+
+
+
 
 //select gender either male or female from form
 let gender = document.querySelectorAll('input[name=gender]');
 console.log(gender);
 
-//Define form=>fillin
-const fillinform = document.querySelector("fillin");
-console.log(fillinform);
+//select form
+const form = document.querySelectorAll("form");
+console.log(form);
 
-let genderM = fillinform.gender_male.value;
-let genderF = fillinform.gender_female.value;
+
+
 
 //validate radio buttons
-
 if(genderM .checked == false && genderF.checked == false){
     console.log("Please choose your Gender either Male or Female");
 }
@@ -24,46 +46,8 @@ if(genderM .checked == false && genderF.checked == false){
 
 
 
-
-
-
-
-// //add eventlistener to form
-// fillinform.addEventListener("submit", validateUserInfo);
-
 // create the function to validate the input when user
 // clicks submit button
-
-function validateUserInfo(event){
-    //to avoid default refresh
-    event.preventDefault();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //creating an array
-// const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-
-
-// //function  to validate 
-
-// function validateForm(event){
-//     event.preventDefault();
-//     const date = document.querySelectorAll("input[name=date]");
-//     alter("form submitted");
-// }
 
 
 
