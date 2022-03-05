aa9f4f4840976d666c17923db44e295317d9ffcf
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-05 18:00:26 +0300

    i write some statemet select date input and give it js format ,select gender input ,add eventlistener to submit ,there after i can incorporate the statement

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-//creating an array
-const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
- 
+//select input of date from form
+let input = document.getElementById("date").value;
+let dateEntered = new Date(input);
+
+//select gender either male or female from form
+let gender = document.querySelectorAll('input[name=gender]');
+
 //Define form=>fillin
-const fillin = document.querySelector(form);
+const fillinform = document.querySelector("fillin");
 
-//add eventlistener to  submit
-const form = form.addEventListener("submit",validateForm);
+//add eventlistener to form
+fillinform.addEventListener("submit", validateUserInfo);
 
+// create the function to validate the input when user
+// clicks submit button
 
-//function  to validate 
-
-function validateForm(event){
-    // event.preventDefault();
-    alter("form submitted")
+function validateUserInfo(event){
+    //to avoid default refresh
+    event.preventDefault();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// //creating an array
+// const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+
+// //function  to validate 
+
+// function validateForm(event){
+//     event.preventDefault();
+//     const date = document.querySelectorAll("input[name=date]");
+//     alter("form submitted");
+// }
 
 
 
