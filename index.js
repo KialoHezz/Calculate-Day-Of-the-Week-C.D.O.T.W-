07bdd26cc467a05a6ee666c7ff5f759c1b4efbbb
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-07 10:38:42 +0300

    incorporate the statement for them to work correct without error

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 //creating an array
 const dayOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const birthday = getElementById('date').value;
+
 //create object
-let Male_object = {unday: "Kwasi" ,Monday: "Kwadwo" ,Tuesday: "Kwabena" ,Wednesday: "Kwaku" ,Thursday:  "Yaw" ,Friday: "Kofi" ,Saturday: "Kwame"};
+let Male_object = {Sunday: "Kwasi" ,Monday: "Kwadwo" ,Tuesday: "Kwabena" ,Wednesday: "Kwaku" ,Thursday:  "Yaw" ,Friday: "Kofi" ,Saturday: "Kwame"};
 let female_object = {Sunday: "Akosua", Monday: "Adwoa", Tuesday: "Abenaa", Wednesday: "Akua", Thursday:  "Yaa", Friday: "Afua", Saturday: "Ama"};
 
 //initilaze form=>fillin
 const fillinform = document.querySelector("#fillin");
 
-
-let gender_female = document.getElementById(genderF).value;
+let gender_female = document.getElementById("genderF");
 console.log(gender_female);
-let gender_male = document.getElementById(genderM).value;
+let gender_male = document.getElementById("genderM");
 console.log(gender_male);
 
 
-document.getElementById("submit").addEventListener("submit", validateUserInfo =>{
-    //to avoid default refresh
-    preventDefault();
-    //select input of date from form
-    let input = document.getElementById("date").value;
-    console.log(input)
+fillinform.addEventListener("submit",validateInfo);
+
+
+function validateInfo(submit){
+    submit.preventDefault();
+    let userDate = document.getElementById("date").value;
+    let gender = document.querySelector("input[name='Gender']:checked")
+    console.log(userDate,gender.value   )
+
     let dateEntered = new Date(input);
     //To ge the day
     const day = dateEntered.getDate();
     return(dayOfTheWeek[day]);
-    // have call the function
-    chose();
-    // correctDate();
-    // correctMonth();
-});
+    
+}
 
 
 
@@ -37,15 +36,11 @@ function chose(male , female){
     male = fillinform.gender_male.value;
     female = fillinform.gender_female.value;
     //validate radio buttons
-    if(male.checked == false && female.checked == false){
+    if(male .checked == false && female.checked == false){
         return("Please choose your Gender either Male or Female");
     }
-      else if (gender_male == "male") {
-        return("You're male");
-    }
 }
-
-
+// chose();
 
 //i have commentted this  two function(correctDate and correctMonth) because i have thought they dont have meaning ;already i have provide calender therefore user will select
 
@@ -63,4 +58,4 @@ function chose(male , female){
 //         return ("Invalid month");
 //     }
 //     return ("Valid month");
-
+// }
